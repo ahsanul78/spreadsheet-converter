30178edb1c2ae0f32b3cd590312c1211bc30131c
unknown <akarim@PR154L.TCICREDIT.LOCAL>
2017-05-10 13:36:51 -0400

    conversion to xml

--- src/main/java/org/ak/spreadsheet/App.java
+++ src/main/java/org/ak/spreadsheet/App.java
@@ -1,10 +1,21 @@
 package org.ak.spreadsheet;
 
+import org.ak.spreadsheet.converter.Converter;
+import org.ak.spreadsheet.converter.ConverterConfig;
+import org.ak.spreadsheet.exceptions.InvalidSpreadSheetException;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ConverterConfig config = new ConverterConfig();
+        Converter converter = new Converter(config);
         
+        try {
+			String xml = converter.toXML("C://programs.xlsx");
+			System.out.println(xml);
+		} catch (InvalidSpreadSheetException e) {
+			e.printStackTrace();
+		}
     }
 }
--- src/main/java/org/ak/spreadsheet/converter/Converter.java
+++ src/main/java/org/ak/spreadsheet/converter/Converter.java
@@ -2,8 +2,19 @@ package org.ak.spreadsheet.converter;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.ak.spreadsheet.exceptions.InvalidSpreadSheetException;
 import org.apache.poi.ss.usermodel.Cell;
@@ -12,12 +23,17 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 public class Converter {
 
+	private static final String NEW_LINE_PATTERN = "\n|\r";
+	private static final String SHEET = "sheet";
+	private static final String WORKBOOK = "workbook";
 	private static final String NEW_LINE = "\r\n";
 	public static final String SPACE = " ";
 	
@@ -73,14 +89,24 @@ public class Converter {
 		} 
 	}
 	
-	private String toXML(String filename) {
-		
-		return "";
+	public String toXML(String filename) throws InvalidSpreadSheetException {
+		try {
+			Workbook wb = WorkbookFactory.create(new File(filename));
+			String xml = convertWorkbookToXML(wb);
+			return xml;
+		} catch (Exception e) {
+			throw new InvalidSpreadSheetException(e.getMessage());
+		} 
 	}
 	
-	private String toXML(InputStream is) {
-		
-		return "";
+	public String toXML(InputStream is) throws InvalidSpreadSheetException {
+		try {
+			Workbook wb = WorkbookFactory.create(is);
+			String xml = convertWorkbookToXML(wb);
+			return xml;
+		} catch (Exception e) {
+			throw new InvalidSpreadSheetException(e.getMessage());
+		} 
 	}
 	
 	private String[] convertWorkbookToCSV(Workbook wb) {
@@ -180,6 +206,68 @@ public class Converter {
 		wbObject.add("sheets", sheetsArray);
 		
 		return wbObject.getAsString();
+	}
+	
+	private String convertWorkbookToXML(Workbook wb) throws InvalidSpreadSheetException, ParserConfigurationException, TransformerException {
+		int sheetCount = wb.getNumberOfSheets();
+		FormulaEvaluator evaluator = null;
+		if(config.isExecuteFormula()) {
+			evaluator = wb.getCreationHelper().createFormulaEvaluator();
+		}
+		
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		
+		// root elements
+		Document doc = docBuilder.newDocument();
+		Element rootElement = doc.createElement(WORKBOOK);
+		doc.appendChild(rootElement);
+		
+		for(int i = 0; i < sheetCount; i++) {
+			Element sheetElement = doc.createElement(SHEET);
+			rootElement.appendChild(sheetElement);
+			
+			Sheet sheet = wb.getSheetAt(i);
+			Row row = null;
+		    Cell cell = null;
+		    Iterator<Row> rowIterator = sheet.iterator();
+		    
+		    ArrayList<String> columns = new ArrayList<String>();
+		    int rowIndex=0;
+		    
+            while (rowIterator.hasNext()) {
+                row = rowIterator.next();
+                if(rowIndex == 0) {
+                	// retrieve column names
+                	columns = ConversionHelper.getColumnHeadings(config, evaluator, row);
+                } else if(rowIndex > 0 && columns.size() > 0){
+                	Element rowElement = doc.createElement("entry");
+                	sheetElement.appendChild(rowElement);
+
+                	Iterator<Cell> cellIterator = row.cellIterator();
+                	int cellIndex = 0;
+    		        while (cellIterator.hasNext()) {
+    		            cell = cellIterator.next();
+    		            Object cellData = ConversionHelper.getCellValue(config, cell, evaluator);
+    		            Element cellElem = doc.createElement(columns.get(cellIndex));
+    		            cellElem.appendChild(doc.createTextNode(String.valueOf(cellData)));
+    		            cellIndex ++;
+    		        }
+                	
+                }
+                
+                rowIndex ++;
+            }
+		   
+		}
+		
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer = tf.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		String output = writer.getBuffer().toString().replaceAll(NEW_LINE_PATTERN, "");
+		return output;
 	}
 
 }
